7ccd95d089a318aad53f1050f4e2e265b489a91d
saurav <sauravpd58@gmail.com>
2020-06-14 00:48:20 +0530

    added Typescript String example

---
+++ basics/demo2.js
@@ -0,0 +1,7 @@
+var str1 = "typescript string example";
+console.log(str1.charAt(1)); // y
+console.log(str1.toUpperCase()); // TYPESCRIPT STRING EXAMPLE
+console.log(str1.toLowerCase()); // typescript string example
+console.log(str1.length); // 25
+console.log(str1.split(" ")); // [ 'typescript', 'string', 'example' ]
+console.log(str1.substring(0, 4));
---
+++ basics/demo2.ts
@@ -0,0 +1,8 @@
+var str1: string="typescript string example"
+
+console.log(str1.charAt(1)) // y
+console.log(str1.toUpperCase()) // TYPESCRIPT STRING EXAMPLE
+console.log(str1.toLowerCase()) // typescript string example
+console.log(str1.length) // 25
+console.log(str1.split(" ")) // [ 'typescript', 'string', 'example' ]
+console.log(str1.substring(0,4)) // type
