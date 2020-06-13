2a357a861ce151b51b0da1676a600022abd03db1
saurav <sauravpd58@gmail.com>
2020-06-13 23:36:42 +0530

    added typescript function example

---
+++ basics/demo1.js
@@ -0,0 +1,5 @@
+function addition(num1, num2) {
+    return num1 + num2;
+}
+var sum = addition(20, 30);
+console.log(sum); // 50
---
+++ basics/demo1.ts
@@ -0,0 +1,6 @@
+function addition(num1:number,num2:number){
+    return num1+num2;
+}
+
+let sum=addition(20,30)
+console.log(sum) // 50
