ddc66d5afa6be9402a3a25561d9fbaa1b29c7364
saurav <sauravpd58@gmail.com>
2020-07-13 03:04:51 +0530

    Added Typescript tuple example

---
+++ tuple/tupleDemo1.js
@@ -0,0 +1,4 @@
+var student = [1111, "Steve"];
+console.log(student);
+console.log(student[0]);
+console.log(student[1]);
---
+++ tuple/tupleDemo1.ts
@@ -0,0 +1,5 @@
+var student:[number,string]=[1111,"Steve"]
+console.log(student)
+
+console.log(student[0])
+console.log(student[1])
