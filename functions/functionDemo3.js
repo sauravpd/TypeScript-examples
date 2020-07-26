2e9656b28ea5c43ae6db7c0f1f371de07e52f344
saurav <sauravpd58@gmail.com>
2020-07-27 01:28:43 +0530

    Added Typescript function example

---
+++ functions/functionDemo3.js
@@ -0,0 +1,4 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log("Addition of two number", add(10, 40));
---
+++ functions/functionDemo3.ts
@@ -0,0 +1,5 @@
+function add(num1:number,num2:number):number {
+    return num1+num2
+}
+
+console.log("Addition of two number",add(10,40))
