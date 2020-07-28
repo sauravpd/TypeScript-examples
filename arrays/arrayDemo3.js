35e835245779eb5e2fa3956aa7effd08941e6fc5
saurav <sauravpd58@gmail.com>
2020-07-29 02:39:42 +0530

    commit changes

---
+++ arrays/arrayDemo3.js
@@ -0,0 +1,10 @@
+var names = ["Steve", "John", "Mike", "Mark"];
+for (var index = 0; index < names.length; index++) {
+    console.log(names[index]);
+}
+/*
+Steve
+John
+Mike
+Mark
+*/ 
---
+++ arrays/arrayDemo3.ts
@@ -0,0 +1,10 @@
+var names:String[]=["Steve","John","Mike","Mark"]
+for (let index = 0; index < names.length; index++) {
+    console.log(names[index])
+}
+/*
+Steve
+John
+Mike
+Mark
+*/
