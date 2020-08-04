ab7585f4da04acd226947d41072e1b5fb7c7ed4e
saurav <sauravpd58@gmail.com>
2020-08-05 02:47:29 +0530

    added typescript arrays index access example

---
+++ arrays/arrayDemo4.js
@@ -0,0 +1,10 @@
+var names = ["Steve", "John", "Mike", "Mark"];
+for (var index in names) {
+    console.log(names[index]);
+}
+/*
+Steve
+John
+Mike
+Mark
+*/ 
---
+++ arrays/arrayDemo4.ts
@@ -0,0 +1,10 @@
+var names: String[] = ["Steve", "John", "Mike", "Mark"]
+for (const index in names) {
+    console.log(names[index])
+}
+/*
+Steve
+John
+Mike
+Mark
+*/
