9dc79745db69ba01bd09e22b103435f7c4af188b
saurav <sauravpd58@gmail.com>
2020-08-11 03:15:19 +0530

    added typescript arrays element access using foreach exmaple

---
+++ arrays/arrayDemo5.js
@@ -0,0 +1,10 @@
+var names = ["Steve", "John", "Mike", "Mark"];
+names.forEach(function (element) {
+    console.log(element);
+});
+/*
+Steve
+John
+Mike
+Mark
+*/ 
---
+++ arrays/arrayDemo5.ts
@@ -0,0 +1,10 @@
+var names:String[]=["Steve","John","Mike","Mark"]
+names.forEach(element => {
+    console.log(element)
+});
+/*
+Steve
+John
+Mike
+Mark
+*/
