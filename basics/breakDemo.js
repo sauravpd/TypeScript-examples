877f5c6e0a040865b56aba5b21fe97070f7aee8b
saurav <sauravpd58@gmail.com>
2020-08-17 03:26:03 +0530

    added typescript break statement example

---
+++ basics/breakDemo.js
@@ -0,0 +1,12 @@
+for (var index = 0; index <= 10; index++) {
+    if (index == 4)
+        break;
+    console.log(index);
+}
+/*
+output
+0
+1
+2
+3
+*/ 
---
+++ basics/breakDemo.ts
@@ -0,0 +1,12 @@
+for (let index = 0; index <= 10; index++) {
+    if (index == 4)
+        break;
+    console.log(index)
+}
+/*
+output
+0
+1
+2
+3
+*/
