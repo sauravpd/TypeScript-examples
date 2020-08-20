7fe44fd792fe4a0c323ba1f1d357a32fc94fed0e
saurav <sauravpd58@gmail.com>
2020-08-21 03:59:13 +0530

    added typescript else conditional statement example

---
+++ basics/elseIfDemo.js
@@ -0,0 +1,13 @@
+var testScore = 85;
+if (testScore >= 80) {
+    console.log("A+ grade");
+}
+else if (testScore > 60 && testScore <= 80) {
+    console.log("A grade");
+}
+else if (testScore >= 40 && testScore < 60) {
+    console.log("B grade");
+}
+else {
+    console.log("fail");
+}
---
+++ basics/elseIfDemo.ts
@@ -0,0 +1,14 @@
+var testScore: number = 85;
+
+if (testScore >=80) {
+    console.log("A+ grade");
+}
+else if (testScore >60 && testScore <=80) {
+    console.log("A grade");
+}
+else if (testScore >= 40 && testScore < 60) {
+    console.log("B grade");
+}
+else {
+    console.log("fail");
+}  
